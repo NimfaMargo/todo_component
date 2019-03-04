d2b25a841119b2a361aeee5189b9c40d6b7b96af
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-04 14:16:33 +0300

    add selector for filtered tasks

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,7 +1,15 @@
 import { createSelector } from 'reselect';
 
-export const getTasks = state => state.tasks;
+export const getTask = state => state.tasks.byId;
+export const getTaskIds = state => state.tasks.allIds;
+export const getFilterName = state => state.tasks.currentFilterName;
 export const tasksSelector = createSelector(
-  getTasks,
-  tasks => Object.values(tasks),
+  getTask,
+  getTaskIds,
+  (task, allIds) => allIds.map(id => task[id]),
+);
+export const filteredTasksSelector = createSelector(
+  tasksSelector,
+  getFilterName,
+  (tasks, filter) => (filter === 'all' ? tasks : tasks.filter(t => t.state === filter)),
 );
