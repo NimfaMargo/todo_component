77e5c18b6f58cbd8a3c9561253546c3d27a4bf36
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-03-04 14:15:15 +0300

    add new components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import NewTaskFormContainer from '../containers/NewTaskForm';
-import TasksListContainer from '../containers/TasksList';
+import NewTaskForm from './NewTaskForm.jsx';
+import Tasks from './Tasks.jsx';
+import Filter from './Filter.jsx';
 
+/* eslint react/react-in-jsx-scope: 0 */
 const App = () => (
   <div className="col-5">
-    <NewTaskFormContainer />
-    <TasksListContainer />
+    <NewTaskForm />
+    <Filter />
+    <Tasks />
   </div>
 );
 export default App;
